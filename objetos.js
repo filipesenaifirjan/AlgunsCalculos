e9713e732ef3155886e2acfc980117d2e3175bda
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-25 09:51:17 -0300

    ex: constructor

--- objetos.js
+++ objetos.js
@@ -3,25 +3,35 @@
 class Pessoa { //classe é a definição
     nome;
     idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2022 - idade;
+    }
+
     descrever() {
         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
     }
 }
-const filipe = new Pessoa();
-filipe.nome = 'Filipe Mattos';
-filipe.idade = 18;
+const filipe = new Pessoa('Filipe', 17);
+//filipe.nome = 'Filipe Mattos';
+//filipe.idade = 18;
 
-const filipe2 = new Pessoa();
-filipe2.nome = 'Filipe2';
-filipe2.idade = 16;
+const filipe2 = new Pessoa('Filipe2', 18);
+//filipe2.nome = 'Filipe2';
+//filipe2.idade = 16;
 
 //duas maneiras abaixo de impromir a classe e as instâncias
-filipe.descrever();
-filipe2.descrever();
-
+//filipe.descrever();
+//filipe2.descrever();
 
 console.log(filipe);
-console.log(filipe2);
+
+
+//console.log(filipe);
+//console.log(filipe2);
 
 
 
