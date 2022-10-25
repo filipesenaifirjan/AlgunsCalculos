8366bcd50c2ade2c2a56a27c7d10d0fa5ef835e2
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-25 09:40:59 -0300

    classe e instância

--- objetos.js
+++ objetos.js
@@ -1,6 +1,37 @@
 
 
-const pessoa = {
+class Pessoa { //classe é a definição
+    nome;
+    idade;
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+const filipe = new Pessoa();
+filipe.nome = 'Filipe Mattos';
+filipe.idade = 18;
+
+const filipe2 = new Pessoa();
+filipe2.nome = 'Filipe2';
+filipe2.idade = 16;
+
+//duas maneiras abaixo de impromir a classe e as instâncias
+filipe.descrever();
+filipe2.descrever();
+
+
+console.log(filipe);
+console.log(filipe2);
+
+
+
+
+
+
+
+
+/*
+const pessoa = {  //instância é a ocorrência
     nome: 'Filipe Mattos',   
     idade: 18,    //outro detalhe bacana é colocar funcão dentro do objeto
 
@@ -11,7 +42,7 @@ const pessoa = {
 };
 
 console.log(pessoa[`nome`]);//através de uma string vc acessa dinamicamente o atributo
-
+*/
 
 
 
