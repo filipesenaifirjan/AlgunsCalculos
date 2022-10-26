460e9193d0766826a3d5dc4d0645154f258a4bc8
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-26 11:15:38 -0300

    calculando numeros pares

--- listas.exercicio.js
+++ listas.exercicio.js
@@ -1,8 +1,25 @@
 //crie um programa que dado um numero, imprima sua tabuada
-
+/*
 const numero = 7;
 for (let i = 1; i <= 10; i++) {
     console.log(i * numero);
    
     
+}*/
+
+//crie um programa que seja capaz de percorrer uma lista de números e imprima cada numero par encontrado
+
+const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+   
+    if (numero % 2 === 0){
+
+        console.log(numero);
+    }
+    
 }
+    
+    
+    
