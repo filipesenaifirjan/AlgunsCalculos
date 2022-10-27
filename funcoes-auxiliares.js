2cc5512241c6195045839405cbf2beb337408dd2
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-27 11:23:04 -0300

    continuação imports exports

--- funcoes-auxiliares.js
+++ funcoes-auxiliares.js
@@ -1,4 +1,7 @@
 
+const entradas = [5, 50, 10, 98, 23];
+let i = 0;
+
 
 function gets() {
 return 10;
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
-const funcoes = require('./funcoes-auxiliares');
+const { gets, print } = require('./funcoes-auxiliares');
 
-console.log(funcoes);
+
+
+//console.log(funcoes.gets());
+/* uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 - 100, Façã um programa que receba os números sorteados para os alunos e mostre o maior número sorteado.
+
+dados de entrada: 5 50 10 98 23 
+saída: 98 */
+
+print(gets());
